2d9776c4bd8eba9400957d50d9764dfa5f4961f6
Eric W. <robo-eric@gmx.de>
2026-01-22 12:49:32 +0100

    implement `stencil_vegas!` using KernelAbstractions.jl (#5)
    * add GPU implementation of VEGAS stencil

--- src/project2.jl
+++ src/project2.jl
@@ -1,13 +1,44 @@
 # TBW
 
+@kernel function _vegas_stencil_kernel!(output_buffer::AbstractVecOrMat, @Const(bins_buffer::AbstractVecOrMat), @Const(sums::AbstractMatrix), @Const(alpha::Real))
+    T = promote_type(eltype(bins_buffer), typeof(alpha))
+    bin_idx, dim_idx = @index(Global, NTuple)
+
+    # smoothing
+    @inbounds smoothed = if bin_idx == firstindex(bins_buffer, 1)
+        (T(7) * bins_buffer[bin_idx, dim_idx] + bins_buffer[nextind(bins_buffer, bin_idx), dim_idx]) / T(8)
+    elseif bin_idx == lastindex(bins_buffer, 1)
+        (T(7) * bins_buffer[bin_idx, dim_idx] + bins_buffer[prevind(bins_buffer, bin_idx), dim_idx]) / T(8)
+    else
+        (T(6) * bins_buffer[bin_idx, dim_idx] + bins_buffer[prevind(bins_buffer, bin_idx), dim_idx] + bins_buffer[nextind(bins_buffer, bin_idx), dim_idx]) / T(8)
+    end
+
+    # normalization
+    normalized = smoothed / @inbounds sums[begin, dim_idx]
+
+    # compression
+    compressed = ((one(T) - normalized) / (log(inv(normalized))))^alpha
+
+    @inbounds output_buffer[bin_idx, dim_idx] = compressed
+end
+
 function stencil_vegas!(backend, bins_buffer::AbstractVecOrMat, alpha::Real)
-    # write the stencil/compression code here
-    # you will need a kernel implementation and pass it the raw vectors/matrices
-    # the bins_buffer is both input and output and should be overwritten with the updated values
-    # example kernels like this are in src/grid.jl: @kernel function _fill_uniformly_kernel
+    if typeof(get_backend(bins_buffer)) != typeof(backend)
+        throw(ArgumentError("buffer does not belong to the passed backend"))
+    end
 
-    # it's a good idea to add some sanity checks too, like asserting that the buffers exist on the same backend, have matching dimensionalities where applicable, etc.
+    bins = size(bins_buffer, 1)
+    dims = size(bins_buffer, 2)
+    if bins < 2
+        throw(ArgumentError("less than two bins specified"))
+    end
 
+    sums = allocate(backend, eltype(bins_buffer), (1, dims))
+    output_buffer = allocate(backend, eltype(bins_buffer), size(bins_buffer))
+
+    sum!(sums, bins_buffer)                 # uses GPU implementation
+    _vegas_stencil_kernel!(backend)(output_buffer, bins_buffer, sums, alpha, ndrange = (bins, dims))
+    copyto!(bins_buffer, output_buffer)     # uses GPU implementation
     return nothing
 end
 
