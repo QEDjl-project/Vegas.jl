a77b846d14577ceaf56a94a5ed3fd783d7f4d25f
camel2go <75307816+Camel2Go@users.noreply.github.com>
2026-01-22 02:22:06 +0100

    adjust plotting + binning sanity check for test

--- test/project1.jl
+++ test/project1.jl
@@ -8,7 +8,7 @@ using Plots
 using StatsPlots
 using DataFrames
 
-plotting = true
+plotting = false
 
 # definition of a simple E = 0, σ = 1 normal distribution
 @inline normal_distribution(u::T) where {T <: Number} = inv(sqrt(T(2) * π)) * exp(-(u^2 / T(2)))
@@ -25,9 +25,7 @@ function testsuite_project1(backend, el_type, nbins, dim)
     LOWER = ntuple(_ -> el_type(0.0), dim)
     UPPER = ntuple(_ -> el_type(1.0), dim)
     
-    println("Hello from Maria and Artur :>")
-
-    @testset "batch_size = $batch_size" for batch_size in (2^10)#, 2^14, 2^18, 2^22)
+    @testset "batch_size = $batch_size" for batch_size in (2^10, 2^14, 2^18, 2^22)
         buffer = allocate_vegas_batch(backend, el_type, dim, batch_size)
         grid = uniform_vegas_grid(backend, LOWER, UPPER, nbins)
         
@@ -58,7 +56,7 @@ function testsuite_project1(backend, el_type, nbins, dim)
                 title = "Sampling Distribution" * plot_details,
                 xlabel = "Sample range [0:1)",
                 ylabel = "Distribution [#]",
-                palette = :seaborn_deep)            
+                palette = :seaborn_deep)
             
 
             savefig("sampling_$(batch_size)_$(dim)_$(el_type).png")
@@ -72,28 +70,34 @@ function testsuite_project1(backend, el_type, nbins, dim)
 
         # == BINNING ==
         bins_buffer = allocate(backend, el_type, (nbins, dim))
-        @test isnothing(binning_vegas!(backend, bins_buffer, buffer, grid, normal_distribution))
+        ndi_buffer = allocate(backend, Int, (nbins, dim))
+
+        @test isnothing(binning_vegas!(backend, bins_buffer, ndi_buffer, buffer, grid, normal_distribution))
 
         if plotting
             println("plotting...")
             binned = zeros(el_type, nbins, dim)
             copyto!(binned, bins_buffer)
             
-            bar(range(0, nbins), binned[:, 1], legend = false, title = "Binning - Samples per Bin" * plot_details, xlabel = "Bin [$nbins]", ylabel = "Amount of samples per bin [#]")
+            bar(range(0, nbins), binned[:, 1], legend = false, title = "Binning" * plot_details, xlabel = "Bin [$nbins]", ylabel = "Average of J²(y)·f²(x(y))")
             
             savefig("binning_$(batch_size)_$(dim)_$(el_type).png")
         end
         
-        if sum(bins_buffer) != batch_size * dim
-            println("Error: Number of binned samples doesnt match!")
-            println(sum(bins_buffer), " / ", batch_size * dim, " = ", sum(bins_buffer) / (batch_size * dim))
-            for d in 1:dim
-                println(bins_buffer[:, d])
-            end
+        # println("bins_buffer:")
+        # for d in 1:dim
+        #     println(bins_buffer[:, d])
+        # end
+
+        if sum(ndi_buffer) != batch_size * dim
+            println("Error: Amount of binned samples wrong, must have lost some in binning kernel!")
+            println(sum(ndi_buffer), " / ", batch_size * dim, " = ", sum(ndi_buffer) / (batch_size * dim))
+            # for d in 1:dim
+            #     println(ndi_buffer[:, d])
+            # end
         end
-    
-        # TODO: add some sanity checks on the results
-        @assert sum(bins_buffer) == batch_size * dim
+        @assert sum(ndi_buffer) == batch_size * dim
+
     end
 
     return
