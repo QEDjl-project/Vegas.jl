364fb21e9f41fd5b9c4a0faf3b61b450a38b370a
lewiski67 <laukidehao@gmail.com>
2026-01-22 13:05:36 +0100

    Add GPU Vegas scan (#9)
    ---------
    
    Co-authored-by: lewiski67 <lauki4396@163.com>
    Co-authored-by: AntonReinhard <anton.reinhard@proton.me>

--- src/project2.jl
+++ src/project2.jl
@@ -1,4 +1,4 @@
-# TBW
+import AcceleratedKernels as AK
 
 @kernel function _vegas_stencil_kernel!(output_buffer::AbstractVecOrMat, @Const(bins_buffer::AbstractVecOrMat), @Const(sums::AbstractMatrix), @Const(alpha::Real))
     T = promote_type(eltype(bins_buffer), typeof(alpha))
@@ -42,17 +42,50 @@ function stencil_vegas!(backend, bins_buffer::AbstractVecOrMat, alpha::Real)
     return nothing
 end
 
-function scan_vegas!(backend, bins_buffer::AbstractVecOrMat)
-    # write the scanning code here
-    # bins_buffer is both input and output, override it with the result
+@kernel function _get_last_col!(lastvals, A, N::Int32, D::Int32)
+    d = @index(Global)
+    if d <= D
+        @inbounds lastvals[d] = A[N, d]
+    end
+end
 
-    # caluclate this value too, take care it has the right element type
+@kernel function _scale_by_invN!(out, lastvals, invN, D::Int32)
+    d = @index(Global)
+    if d <= D
+        @inbounds out[d] = lastvals[d] * invN
+    end
+end
+
+function scan_vegas!(backend, bins_buffer::AbstractVecOrMat)
     T = eltype(bins_buffer)
-    avg_d = T(0.0)
+
+    # Handle the vector case
+    A = bins_buffer isa AbstractVector ? reshape(bins_buffer, :, 1) : bins_buffer
+
+    N = Int32(size(A, 1))
+    D = Int32(size(A, 2))
+    # 1) Inclusive scan per column (in-place) using AcceleratedKernels
+    #    dims=1 means scanning along the first dimension (column-wise)
+    AK.accumulate!(+, A, A; dims = 1, init = zero(T))
+
+    # 2) Get the last value of each column
+    lastvals = similar(A, T, (Int(D),))
+    kernel_last = _get_last_col!(backend)
+    kernel_last(lastvals, A, N, D; ndrange = Int(D))
+    KernelAbstractions.synchronize(backend)
+
+    # 3) Compute avg_d = lastvals / N
+    avg_d = similar(A, T, (Int(D),))
+    invN = T(1) / T(N)
+    kernel_scale = _scale_by_invN!(backend)
+    kernel_scale(avg_d, lastvals, invN, D; ndrange = Int(D))
+    KernelAbstractions.synchronize(backend)
+
     return avg_d
 end
 
-function refine_vegas!(backend, grid::VegasGrid, bins_buffer::AbstractVecOrMat, avg_d::Real)
+
+function refine_vegas!(backend, grid::VegasGrid, bins_buffer::AbstractVecOrMat, avg_d::AbstractVector)
     # write the refining code here
     # grid is both input and output, override it with the result
 
