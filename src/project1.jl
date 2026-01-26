cd96c70ca6cd9a496545345a0a0762c9bf558d77
camel2go <75307816+Camel2Go@users.noreply.github.com>
2026-01-26 17:51:06 +0100

    add unbatched binning kernel as fallback (Device too old / Kernel doesnt support @atomic)

--- src/project1.jl
+++ src/project1.jl
@@ -9,7 +9,7 @@ using Atomix: @atomic
     for d in 1:D
         
         # yn = randoms[i, d] * (Ng - 1)         # would be used instead of next line in case of host generated randoms
-        yn = (rand() % 1) * (Ng - 1)            # generate random float in [0:1), scale it to grid
+        yn = (rand() % 1) * (Ng - 1)           # generate random float in [0:1), scale it to grid
         yi = unsafe_trunc(Int, yn) + 1          # use its integer part as bin index
         yd = yn + 1 - yi                        # and its fractional part as shift inside the bin
         
@@ -53,13 +53,14 @@ function sample_vegas!(backend, buffer::VegasBatchBuffer{T, N, D, V, W, J}, grid
     return nothing
 end
 
-@kernel function vegas_binning_kernel!(bins_buffer::AbstractMatrix{T}, ndi_buffer::AbstractMatrix{Int}, values::AbstractMatrix{T}, grid_lines, func::Function, @Const(Ng), ::Val{D}) where {T<:Number, D}
+
+@kernel function vegas_binning_kernel_batched!(bins_buffer::AbstractMatrix{T}, ndi_buffer::AbstractMatrix{Int}, values::AbstractMatrix{T}, grid_lines::AbstractMatrix{T}, func::Function, @Const(Ng), ::Val{D}) where {T<:Number, D}
 
     bin, dim, batch = @index(Global, NTuple)
     batch -= 1
 
-    bin_sum = 0
-    ndi = 0
+    bin_sum = zero(T)
+    ndi = zero(T)
 
     lower_bound = grid_lines[bin, dim]
     upper_bound = grid_lines[bin + 1, dim]
@@ -73,26 +74,60 @@ end
 
     for sample in batch_start:batch_end
 
-        in_bin = if is_last_bin
-            lower_bound <= values[sample, dim] <= upper_bound
-        else
-            lower_bound <= values[sample, dim] < upper_bound
-        end
-        
-        if in_bin
-        # if lower_bound <= values[sample, dim] < upper_bound || (is_last_bin && values[sample, dim] == upper_bound)
+        # TODO: with high sample counts some samples fall _on_ the last grid line, despite random sampling excludes 1
+        if lower_bound <= values[sample, dim] < upper_bound || (is_last_bin && values[sample, dim] == upper_bound)
             V = ntuple(d -> (@inbounds values[sample, d]), Val(D))
             bin_sum += func(V)^2
             ndi += 1
         end
     end
 
+    # could happen that for our batch no samples fall into the bin
+    if ndi != 0
+
+        # jacobian is the same for all samples in this bin/dim, so no need to sum it up
+        jac = Ng * (grid_lines[bin + 1, dim] - grid_lines[bin, dim])
+        @atomic bins_buffer[bin, dim] += (jac^2) / ndi * bin_sum / threads_per_bin
+
+        # used for sanity check that all samples are binned and none is lost
+        @atomic ndi_buffer[bin, dim] += ndi
+    end
+
+end
+
+@kernel function vegas_binning_kernel!(bins_buffer::AbstractMatrix{T}, ndi_buffer::AbstractMatrix{Int}, values::AbstractMatrix{T}, grid_lines::AbstractMatrix{T}, func::Function, @Const(Ng), ::Val{D}) where {T<:Number, D}
+
+    nbins = Ng - 1
+
+    # this is why we cant have nice things
+    i = @index(Global) - 1
+    bin = (i % nbins) + 1
+    dim = div(i, nbins) + 1
+    
+    ndi = zero(T)
+    bins_buffer[bin, dim] = zero(T)
+
+    lower_bound = grid_lines[bin, dim]
+    upper_bound = grid_lines[bin + 1, dim]
+
+    is_last_bin = bin == Ng - 1
+
+    for sample in 1:size(values, 1)
+
+        # TODO: with high sample counts some samples fall _on_ the last grid line, despite random sampling excludes 1
+        if lower_bound <= values[sample, dim] < upper_bound || (is_last_bin && values[sample, dim] == upper_bound)
+            ndi += 1
+            V = ntuple(d -> (@inbounds values[sample, d]), Val(D))
+            bins_buffer[bin, dim] += func(V) ^ 2
+        end
+    end
+
     # jacobian is the same for all samples in this bin/dim, so no need to sum it up
     jac = Ng * (grid_lines[bin + 1, dim] - grid_lines[bin, dim])
-    @atomic bins_buffer[bin, dim] += (jac^2) / ndi * bin_sum / threads_per_bin
-
-    # used for sanity check that all samples are binned and none is lost
-    @atomic ndi_buffer[bin, dim] += ndi
+    
+    bins_buffer[bin, dim] *= (jac ^ 2) / ndi
+    
+    ndi_buffer[bin, dim] = ndi  # used for sanity check that all samples are binned and none is lost
 
 end
 
@@ -106,14 +141,24 @@ function binning_vegas!(backend, bins_buffer::AbstractMatrix{T}, ndi_buffer::Abs
 
     nbins = Ng - 1
 
-    # need to be zeroed because every thread just adds its calculation
-    fill!(bins_buffer, 0)
-    fill!(ndi_buffer, 0)
-
-    @debug "Calling binning kernel with $(nbins) bins * $(D) dims * $(threads_per_bin) threads/bin = $(nbins * D * threads_per_bin) threads"
-
-    vegas_binning_kernel!(backend)(bins_buffer, ndi_buffer, buffer.values, grid.nodes, func, Ng, Val(D), ndrange = (nbins, D, threads_per_bin))
+    # NOTE: in case your GPU doesnt support @atomic for the selected float, change to true to use unbatched implementation
+    if false
+        
+        @debug "Calling unbatched binning kernel with $(nbins) bins * $(D) dims = $(nbins * D) threads"
+        vegas_binning_kernel!(backend)(bins_buffer, ndi_buffer, buffer.values, grid.nodes, func, Ng, Val(D), ndrange = nbins * D)
     
+    else
+        
+        # need to be zeroed because every thread just adds its calculation
+        fill!(bins_buffer, zero(T))
+        fill!(ndi_buffer, zero(T))
+    
+        @debug "Calling batched binning kernel with $(nbins) bins * $(D) dims * $(threads_per_bin) threads/bin = $(nbins * D * threads_per_bin) threads"
+
+        vegas_binning_kernel_batched!(backend)(bins_buffer, ndi_buffer, buffer.values, grid.nodes, func, Ng, Val(D), ndrange = (nbins, D, threads_per_bin))
+    
+    end
+
     synchronize(backend)
 
     return nothing
